c016fc0ee94c56da16d0350d085dfd8624e6035a
chermat mustapha <mchermat@outlook.fr>
2025-11-24 18:51:04 +0100

    change list to List from typing

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 """Module that defines a function
 to sum a list of mixed integers and floats."""
-from typing import Union
+from typing import Union, List
 
 
-def sum_mixed_list(mxd_lst: list[Union[int, float]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Return the sum of a list of mixed integers and floats.
 
     Args:
-        mxd_lst (list[Union[float, int]]): A list of integers and floats.
+        mxd_lst (List[Union[float, int]]): A list of integers and floats.
 
     Returns:
         float: The sum of all the integers and floats in the list.

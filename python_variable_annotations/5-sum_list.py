4dc350bf765afdae4fa196c693010297326d978d
chermat mustapha <mchermat@outlook.fr>
2025-11-24 18:49:43 +0100

    pycodestyle

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """Module that defines a function to
 sum a list of floats with type annotations."""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Returns the sum of all the elements of a list of floats.
 
     Args:
-        input_list (list[float]): A list of floats.
+        input_list (List[float]): A list of floats.
 
     Returns:
         float: The sum of all the elements of the list.

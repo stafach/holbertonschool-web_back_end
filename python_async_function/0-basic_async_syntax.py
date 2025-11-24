51e30cddcbe95ede120461b1983d7ad15107ae5f
chermat mustapha <mchermat@outlook.fr>
2025-11-24 21:22:19 +0100

    add type for x and return type for wait_random

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
-"""Module that define a function
-asynchron"""
+"""Module that defines an asynchronous function"""
+
 import asyncio
 import random
 
 
-async def wait_random(max_delay: int = 10):
-    """Fonction that wait a random time between
-     0 and 10 and return it"""
-    x = random.uniform(0.0, max_delay)
+async def wait_random(max_delay: int = 10) -> float:
+    """Function that waits a random time between
+    0 and max_delay seconds and returns it"""
+
+    x: float = random.uniform(0, max_delay)
     await asyncio.sleep(x)
     return x

fa21688f6943303428080f30cecc189f751e4ba0
chermat mustapha <mchermat@outlook.fr>
2025-11-24 18:04:49 +0100

    Write a type-annotated function concat that takes a string str1 and a string str2 as arguments and returns a concatenated string'

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module that defines a function
+to concatenate two strings with type annotations."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenates two strings and returns the result."""
+    return str1 + str2

06bc6ef6166dccf2df49e683dd95aef0c2579c51
chermat mustapha <mchermat@outlook.fr>
2025-11-24 22:05:19 +0100

    add type for i

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -9,6 +9,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Function that return a list of max_delay"""
+    i : int = 0
     coroutines = [wait_random(max_delay) for i in range(n)]
     delays: List[float] = []
 

1ff0cfe391314fc8b4700b01d592e60daed50750
chermat mustapha <mchermat@outlook.fr>
2025-11-25 11:13:09 +0100

    change function

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -10,8 +10,6 @@ async def async_generator() -> AsyncGenerator[float, None]:
     """Function that will loop 10 times, each
     time asynchronously wait 1 second, then
     yield a random number between 0 and 10"""
-    i: int = 0
-    for i in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
-        delay: float = random.uniform(0, 10)
-        yield delay
+        yield random.uniform(0, 10)

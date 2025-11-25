3f4eb30c69a22db4eea741ec8858dede5c22daeb
chermat mustapha <mchermat@outlook.fr>
2025-11-25 11:04:10 +0100

    add return type

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,9 +3,10 @@
 
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float]:
     """Function that will loop 10 times, each
     time asynchronously wait 1 second, then
     yield a random number between 0 and 10"""

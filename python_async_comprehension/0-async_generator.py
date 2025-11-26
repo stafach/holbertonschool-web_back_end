3e16dbdcf37e1a135542ff074d85fc1d39bfa9b4
chermat mustapha <mchermat@outlook.fr>
2025-11-26 11:54:27 +0100

    reduce function

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -13,7 +13,6 @@ async def async_generator() -> AsyncGenerator[float, None]:
     then yields a random float between 0 and 10.
     """
     i: int = 0
-    for i in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
-        delay: float = random.uniform(0, 10)
-        yield delay
+        yield random.uniform(0, 10)

b30c814a2cfce4d6b1c62a85c47f7af1d1bcb06c
chermat mustapha <mchermat@outlook.fr>
2025-11-26 11:48:00 +0100

    change function

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -10,9 +10,4 @@ async_generator = __import__('0-async_generator').async_generator
 async def async_comprehension() -> List[float]:
     """Corutine that collect 10 random numbers
     then return the 10 random numbers."""
-    result: List[float] = []
-
-    async for i in async_generator():
-        result.append(i)
-
-    return result
+    return [i async for i in async_generator()]

10755b21b065a407cce7162b962a9c0f01de18cc
chermat mustapha <mchermat@outlook.fr>
2025-12-12 00:53:08 +0100

    Implement a class named Car:
    Constructor attributes:
    brand (String)
    motor (String)
    color (String)
    Each attribute must be stored in an “underscore” attribute version (ex: name is stored in _name)
    Add a method named cloneCar. This method should return a new object of the class.

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,21 @@
+export default class Car {
+    constructor(brand, motor, color) {
+        if (typeof brand !== "string") {
+            throw new TypeError("Brand must be a string");
+        }
+        if (typeof motor !== "string") {
+            throw new TypeError("Motor must be a string");
+        }
+        if (typeof color !== "string") {
+            throw new TypeError("Color must be a string");
+        }
+
+        this._brand = brand;
+        this._motor = motor;
+        this._color = color;
+    }
+
+    cloneCar() {
+        return new this[_constructor](this._brand, this._motor, this._color);
+    }
+}

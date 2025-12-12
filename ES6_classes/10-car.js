ca4c5edadc200b9610e955c5541fa2b9589d1dbd
chermat mustapha <mchermat@outlook.fr>
2025-12-12 01:06:23 +0100

    change method

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,24 +1,21 @@
-const _constructor = Symbol('constructor');
-
 export default class Car {
     constructor(brand, motor, color) {
-        if (typeof brand !== "string") {
+        if (brand !== undefined && typeof brand !== "string") {
             throw new TypeError("Brand must be a string");
         }
-        if (typeof motor !== "string") {
+        if (motor !== undefined && typeof motor !== "string") {
             throw new TypeError("Motor must be a string");
         }
-        if (typeof color !== "string") {
+        if (color !== undefined && typeof color !== "string") {
             throw new TypeError("Color must be a string");
         }
 
         this._brand = brand;
         this._motor = motor;
         this._color = color;
-        this[_constructor] = this.constructor;
     }
 
     cloneCar() {
-        return new this[_constructor](this._brand, this._motor, this._color);
+        return new this.constructor();
     }
 }

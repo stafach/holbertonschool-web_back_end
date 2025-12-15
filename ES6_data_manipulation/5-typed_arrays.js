a38f4bbe97b978200035d8ecf857fb39ef719739
chermat mustapha <mchermat@outlook.fr>
2025-12-15 02:21:09 +0100

    learn int8

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,13 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw new Error("Position outside range");
+  }
+
+  const buffer = new ArrayBuffer(length);
+
+  const int8 = new Int8Array(buffer);
+
+  int8[position] = value;
+
+  return buffer;
+}

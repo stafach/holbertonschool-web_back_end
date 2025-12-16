b25cc751cc6d4c2b1ef6cb5bd084a7c4dfc322ef
chermat mustapha <mchermat@outlook.fr>
2025-12-16 11:40:10 +0100

    change error to string

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -6,7 +6,7 @@ export default function guardrail(mathFunction) {
         queue.push(test);
     }
     catch (error) {
-        queue.push(error);
+        queue.push(String(error));
     }
     finally {
         queue.push('Guardrail was processed');

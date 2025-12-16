07e0922fce5a32ca876120a744cf336f28e6bc2c
chermat mustapha <mchermat@outlook.fr>
2025-12-16 10:31:50 +0100

    change reject

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -3,6 +3,6 @@ export default function getFullResponseFromAPI(success) {
         success ? resolve({
             status: 200,
             body: 'Success'
-        }): reject("The fake API is not working currently");
+        }): reject(new Error("The fake API is not working currently"));
     })
 }

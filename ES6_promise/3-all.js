b948f19049347085d1ed777d299f8a0e1e1a7f6b
chermat mustapha <mchermat@outlook.fr>
2025-12-16 10:55:18 +0100

    add try and await

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,9 +1,10 @@
-import {uploadPhoto, createUser} from './utils.js'
+import { uploadPhoto, createUser } from './utils.js';
 
-export default function handleProfileSignup() {
-    Promise.all([uploadPhoto(), createUser()])
-    .then(([photo, user]) => {
-        console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
-    })
-    .catch(error => console.error("Signup system offline"));
+export default async function handleProfileSignup() {
+  try {
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+    console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+  } catch (error) {
+    console.error("Signup system offline");
+  }
 }

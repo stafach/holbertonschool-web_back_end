5e5480d01a096f52ce6ddb7fb398e2f6ac2b7438
chermat mustapha <mchermat@outlook.fr>
2025-12-16 11:13:39 +0100

    learn allSettled

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,16 @@
+import signUpUser from './4-user-promise'
+import uploadPhoto from './5-photo-reject'
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+
+  const results = await Promise.allSettled([userPromise, photoPromise]);
+    return results.map(result => {
+        if (result.status === 'fulfilled') {
+        return { status: 'fulfilled', value: result.value };
+        } else {
+        return { status: 'rejected', value: result.reason };
+        }
+    });
+}

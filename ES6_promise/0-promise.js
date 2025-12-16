b39ed910b165be2d7f7b7082c23a9142aed88667
chermat mustapha <mchermat@outlook.fr>
2025-12-16 10:23:59 +0100

    change function

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,11 +1,3 @@
 export default function getResponseFromAPI() {
-const myPromise = new Promise((resolve, reject) => {
-    const success = true;
-    if (success) {
-        return success;
-    } else {
-        return false;
-    }
-});
-return myPromise;
+  return Promise.resolve(true);
 }
